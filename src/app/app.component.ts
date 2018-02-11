67ae58d70020d1914ecfe9865b884b94d0ca10a5
Dolan <dolan_miu@hotmail.com>
2018-02-11 21:51:05 +0000

    Add own flavours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+    public title: string;
+
+    constructor() {
+        this.title = 'app';
+    }
 }
