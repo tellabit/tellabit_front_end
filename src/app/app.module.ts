7d2bc69efc59a6d288ed2e782d169b99a57210bb
Dolan <dolan_miu@hotmail.com>
2018-02-11 22:42:53 +0000

    Add scaffolding for home and team pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TeamModule } from './team/team.module';
 
 @NgModule({
     declarations: [AppComponent, NavBarComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, AppRoutingModule, HomeModule, TeamModule],
     providers: [],
     bootstrap: [AppComponent],
 })
