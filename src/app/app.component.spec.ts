0b4a9d4012a8a68e0a313df4de2a557f06b737f2
Dolan <dolan_miu@hotmail.com>
2018-02-11 22:51:18 +0000

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -8,6 +9,7 @@ describe('AppComponent', () => {
         async(() => {
             TestBed.configureTestingModule({
                 declarations: [AppComponent, NavBarComponent],
+                imports: [RouterTestingModule],
             }).compileComponents();
         }),
     );
