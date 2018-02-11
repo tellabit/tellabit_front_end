f53262db4403e951dee891d32a2879cb78d58e44
Dolan <dolan_miu@hotmail.com>
2018-02-11 22:14:31 +0000

    Add nav to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    public title: string;
-
-    constructor() {
-        this.title = 'app';
-    }
-}
+export class AppComponent {}
