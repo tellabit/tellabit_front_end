bc962cda0badd40355f79001ff3c6d462347f192
Dolan <dolan_miu@hotmail.com>
2018-02-19 03:20:09 +0000

    Add fixed navigation bar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer } from '@angular/core';
 
 @Component({
     selector: 'app-nav-bar',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
     styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
-    constructor() {}
+    public scrolled: boolean;
+
+    constructor(renderer: Renderer) {
+        renderer.listenGlobal('window', 'scroll', () => {
+            const scrollTop =
+                window.pageYOffset ||
+                document.documentElement.scrollTop ||
+                document.body.scrollTop ||
+                0;
+            this.scrolled = scrollTop > 60 ? true : false;
+            console.log(scrollTop);
+        });
+    }
 }
